b58b4b70c1139969adb31e65e337f68e72ce0048
renat <renat0014@gmail.com>
2023-11-21 09:11:16 +0100

    feat: update example app

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,53 @@
-import { useState } from 'react'
 import './App.css'
-import { anonymous } from '@streamlayer/sdk-web-anonymous-auth'
-import { StreamLayerProvider, StreamLayerSDKReact, useStreamLayer } from '@streamlayer/react'
+import { useState } from 'react'
+import { MastersStreamLayerProvider, MastersStreamLayerSDKReact } from '@streamlayer/react/masters'
 import { StreamLayerSDKPoints } from '@streamlayer/react/points'
+import { StreamLayerLogin } from '@streamlayer/react/auth'
 import '@streamlayer/react/style.css'
 
-const EventInput = () => {
-  const sdk = useStreamLayer()
-  const [event, setEvent] = useState('')
-  const startEventSession = () => {
-    sdk?.createEventSession(event)
-  }
-
-  if (!sdk) {
-    return null
-  }
-
-  return (
-    <div>
-      <input type="text" value={event} onChange={e => setEvent(e.target.value)} />
-      <button onClick={startEventSession}>set</button>
-    </div>
-  )
-}
-
-const plugins = new Set([anonymous])
-
 function App() {
+  const [user, setUser] = useState({ token: '', schema: '' })
+
+  const submitUser = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault()
+    const data = new FormData(e.currentTarget)
+
+    const token = data.get('token') as string
+    const schema = data.get('schema') as string
+
+    setUser({ token, schema })
+  }
+
   return (
-    <StreamLayerProvider plugins={plugins} sdkKey={process.env.VITE_SDK_KEY || ''}>
-      <EventInput />
-      <StreamLayerSDKPoints />
-      <StreamLayerSDKReact />
-    </StreamLayerProvider>
+    <div className='app-div'>
+      <form className='auth-form' onSubmit={submitUser}>
+        <div>
+          <label htmlFor="token">token</label>
+          <input type="text" id="token" name="token" />
+        </div>
+        <div>
+          <label htmlFor="schema">schema</label>
+          <input type="text" id="schema" name="schema" />
+        </div>
+        <div>
+          <button type="submit">submit</button>
+        </div>
+      </form>
+      <MastersStreamLayerProvider sdkKey={process.env.VITE_SDK_KEY || ''}>
+        <div className='points'>
+          <StreamLayerSDKPoints />
+        </div>
+        <StreamLayerLogin token={user.token} schema={user.schema} />
+        <MastersStreamLayerSDKReact>
+            {({ enableSdk, disableSdk }) => (
+              <div>
+                <button onClick={() => enableSdk('733')}>enable</button>
+                <button onClick={disableSdk}>disable</button>
+              </div>
+            )}
+          </MastersStreamLayerSDKReact>
+      </MastersStreamLayerProvider>
+    </div>
   )
 }
 
